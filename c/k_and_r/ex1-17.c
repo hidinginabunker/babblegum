7bcc0a757e930baed425b8b554eb8b6a266797a9
Gabriel Herrera <gherrera@spotify.com>
2013-04-30 20:07:01 -0400

    Added another exercise to k&r

---
+++ c/k_and_r/ex1-17.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * Write a program to print all input lines that are longer than 80 characters
+ */
+
+//TODO
+// create a function called get_line and another function copy
+
+#define LINE_MIN  80
+#define LINE_LIM  140
+
+int main() {
+  int c, i;
+  char line[LINE_LIM];
+
+  i = 0;
+  while((c = getchar()) != EOF) {
+
+  }
+}
