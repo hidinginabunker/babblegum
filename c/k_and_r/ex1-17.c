4fd8769971d1c80f9f0a0f1e93ab805d9f37f2d6
Gabriel Herrera <gherrera@spotify.com>
2013-05-08 00:24:38 -0400

    Added solution for ex1-17 in k&r

--- c/k_and_r/ex1-17.c
+++ c/k_and_r/ex1-17.c
@@ -1,20 +1,66 @@
 #include <stdio.h>
 /*
  * Write a program to print all input lines that are longer than 80 characters
+ * TODO: Fix for lines longer than line_lim, it doesn't stop after reaching an EOF
  */
 
-//TODO
-// create a function called get_line and another function copy
-
 #define LINE_MIN  80
-#define LINE_LIM  140
+#define LINE_LIM  160
+
+int get_line(char s[], int lim);
+int print_line(char s[], int len);
 
 int main() {
-  int c, i;
+  int c, i, len;
   char line[LINE_LIM];
+  int in_long_line = 0;
 
-  i = 0;
-  while((c = getchar()) != EOF) {
+  // loop through the stream until we have reached the EOF
 
+  while ((len = get_line(line, LINE_LIM)) > 0) {
+
+    if (len > LINE_MIN) {
+      in_long_line = 1;
+    }
+
+    if (in_long_line) {
+      print_line(line, len);
+    }
+
+    if(line[len-1] == '\n') {
+      in_long_line = 0;
+    }
+
+  }
+}
+
+
+
+int get_line(char s[], int lim) {
+  int c, i;
+
+  for (i=0; i < lim-1 && (c = getchar()) != EOF && c != '\n'; i++) {
+    s[i] = c;
+  }
+
+  if (c == '\n') {
+    s[i] = c;
+    i++;
+  }
+
+  s[i] = '\0';
+  return i;
+}
+
+
+
+int print_line(char s[], int len) {
+  int i;
+  for(i=0; i < len; i++) {
+    putchar(s[i]);
+  }
+
+  if( s[i] != '\0') {
+    putchar(s[i]);
   }
 }
